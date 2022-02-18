1c917347c4fcaabf4ef91f8f474a855657559222
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-18 17:38:38 +0800

    Create helpers for fetching Popular TV API

--- src/api/TmdbApi.ts
+++ src/api/TmdbApi.ts
@@ -2,6 +2,7 @@ import {
   API_URL,
   API_KEY,
   POPULAR_MOVIES_URL,
+  POPULAR_TVS_URL,
 } from '../constants/TmdbApiConstants';
 
 export type Movie = {
@@ -18,9 +19,27 @@ export type Movies = {
   total_results: number;
 };
 
+export type Tv = {
+  id: number;
+  original_title: string;
+  poster_path: string;
+  title: string;
+};
+
+export type Tvs = {
+  page: number;
+  results: Tv[];
+  total_pages: number;
+  total_results: number;
+};
+
 const API = {
   fetchPopularMovies: async () : Promise<Movies> => {
     const endpoint: string = `${POPULAR_MOVIES_URL}`;
+    return await (await fetch(endpoint)).json();
+  },
+  fetchPopularTvs: async () : Promise<Tvs> => {
+    const endpoint: string = `${POPULAR_TVS_URL}`;
     return await (await fetch(endpoint)).json();
   }
 }
--- src/constants/TmdbApiConstants.ts
+++ src/constants/TmdbApiConstants.ts
@@ -2,6 +2,7 @@ const API_URL: string = "https://api.themoviedb.org/3/";
 const API_KEY: string | undefined = process.env.REACT_APP_TMDB_API_KEY;
 
 const POPULAR_MOVIES_URL: string = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`
+const POPULAR_TVS_URL: string = `${API_URL}tv/popular?api_key=${API_KEY}&language=en-US`
 
 const IMAGE_BASE_URL: string = 'http://image.tmdb.org/t/p/w780';
 
@@ -9,5 +10,6 @@ export {
   API_URL,
   API_KEY,
   POPULAR_MOVIES_URL,
+  POPULAR_TVS_URL,
   IMAGE_BASE_URL,
 };
