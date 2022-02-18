37c51d0d680f54f2eb4f59f8e7d8a686232b40ea
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-18 17:05:28 +0800

    Fetch popular movies in Home component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
 
+import { IMAGE_BASE_URL } from '../../constants/TmdbApiConstants';
+
+import usePopularMovieFetch from '../../hooks/usePopularMovieFetch';
+import Thumbnail from '../Thumbnail';
+
 const Home: React.FC = () => {
+
+  const { state, loading, error } = usePopularMovieFetch();
+  console.log(state);
+
+  if (error) {
+    return (
+      <div className='mt-5 sm:mt-10'>
+        <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
+          <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
+            <h1 className='text-4xl sm:text-5xl text-left font-bold'>Something went wrong...</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className='mt-5 sm:mt-10'>
-      <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
-        <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
+    <div className='mt-10 py-10 sm:py-20 px-4 sm:px-16 md:px-32 xl:px-80 font-mono'>
+      <div className=''>
+        <div>
           <h1 className='text-4xl sm:text-5xl text-left font-bold'>Popular Movies</h1>
         </div>
-        <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40'>
+        <div className='flex overflow-x-auto'>
+          {state.results.map(movie => (
+            <Thumbnail
+              key={movie.id}
+              source={`${IMAGE_BASE_URL}${movie.poster_path}`} />
+          ))
+          }
+        </div>
+      </div>
+      <div className='mt-5'>
+        <div>
           <h1 className='text-4xl sm:text-5xl text-left font-bold'>Popular TV Shows</h1>
         </div>
       </div>
---
+++ src/components/Thumbnail/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+  source: string;
+};
+
+const Thumbnail : React.FC<Props> = ({ source }) => {
+  return (
+    <img className='h-80 md:h-100 mx-8 my-5 shadow border-4 hover:scale-105' src={source} />
+  );
+}
+
+export default Thumbnail;
