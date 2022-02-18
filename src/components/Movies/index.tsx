c57754e5c756d6b219546b171042aea954034fe7
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-18 15:30:09 +0800

    Add movie component placeholder

---
+++ src/components/Movies/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Movies: React.FC = () => {
+  return (
+    <div className='mt-5 sm:mt-10'>
+      <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
+        <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
+          <h1 className='text-4xl sm:text-5xl text-left font-bold'>Movies</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Movies;
