fc1e24746e9f1cf44a52e4e19ba8b4ae5ce73311
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-18 17:46:37 +0800

    Move popular movies to a component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,45 +1,14 @@
 import React from 'react';
 
-import { IMAGE_BASE_URL } from '../../constants/TmdbApiConstants';
-
-import usePopularMovieFetch from '../../hooks/usePopularMovieFetch';
-
 // Components
+import PopularMovies from '../PopularMovies';
 import PopularTvs from '../PopularTvs';
-import Thumbnail from '../Thumbnail';
 
 const Home: React.FC = () => {
 
-  const { state, loading, error } = usePopularMovieFetch();
-  console.log(state);
-
-  if (error) {
-    return (
-      <div className='mt-5 sm:mt-10'>
-        <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
-          <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
-            <h1 className='text-4xl sm:text-5xl text-left font-bold'>Something went wrong...</h1>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className='mt-10 py-10 sm:py-20 px-4 sm:px-16 md:px-32 xl:px-80 font-mono'>
-      <div className=''>
-        <div>
-          <h1 className='text-4xl sm:text-5xl text-left font-bold'>Popular Movies</h1>
-        </div>
-        <div className='flex overflow-x-auto'>
-          {state.results.map(movie => (
-            <Thumbnail
-              key={movie.id}
-              source={`${IMAGE_BASE_URL}${movie.poster_path}`} />
-          ))
-          }
-        </div>
-      </div>
+    <div className='mt-10 py-10 sm:py-20 px-4 sm:px-16 md:px-32 lg:px-80 font-mono'>
+      <PopularMovies />
       <PopularTvs />
     </div>
   );
---
+++ src/components/PopularMovies/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+// Constants
+import { IMAGE_BASE_URL } from '../../constants/TmdbApiConstants';
+
+// Hooks
+import usePopularMovieFetch from '../../hooks/usePopularMovieFetch';
+
+// Components
+import Thumbnail from '../Thumbnail';
+
+const PopularMovies : React.FC = () => {
+  const { state, loading, error } = usePopularMovieFetch();
+  console.log(state);
+
+  if (error) {
+    return (
+      <div className='mt-5 sm:mt-10'>
+        <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
+          <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
+            <h1 className='text-4xl sm:text-5xl text-left font-bold'>Something went wrong...</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className=''>
+    <div>
+      <h1 className='text-4xl sm:text-5xl text-left font-bold'>Popular Movies</h1>
+    </div>
+    <div className='flex overflow-x-auto'>
+      {state.results.map(movie => (
+        <Thumbnail
+          key={movie.id}
+          source={`${IMAGE_BASE_URL}${movie.poster_path}`} />
+      ))
+      }
+    </div>
+  </div>
+  )
+}
+
+export default PopularMovies;
