38ab58c518393d8a50d957476446010276e0afdf
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-18 15:13:06 +0800

    Add home component placeholder

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className='mt-5 sm:mt-10'>
+      <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
+        <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
+          <h1 className='text-4xl sm:text-5xl text-left font-bold'>Popular Movies</h1>
+        </div>
+        <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40'>
+          <h1 className='text-4xl sm:text-5xl text-left font-bold'>Popular TV Shows</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
