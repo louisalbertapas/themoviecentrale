4484d603997e0c6d60c9e9922f4f151763ff0a36
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-23 16:57:46 +0800

    Create hooks for Tv shows home

---
+++ src/hooks/useTvShowsHomeFetch.ts
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+import API, { Tv } from '../api/TmdbApi';
+
+const initialState = {
+  page: 0,
+  results: [] as Tv[],
+  total_pages: 0,
+  total_results: 0,
+}
+
+const useTvShowsHomeFetch = () => {
+  const [ state, setState ] = useState(initialState);
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState(false);
+  const [ searchText, setSearchText ] = useState("");
+
+  const fetchTvShows = async (page: number, searchText = "", includeAdult = false) => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const tvshows = await API.searchTvShows(page, searchText, includeAdult);
+
+      setState(prev => ({
+        ...tvshows,
+        results: page > 1
+        ? [...prev.results, ...tvshows.results]
+        : [...tvshows.results]
+      }));
+
+    } catch (error) {
+      setError(true);
+    }
+
+    setLoading(false);
+  }
+
+  // Initial load
+  useEffect(() => {
+    fetchTvShows(1, searchText);
+  }, [searchText])
+
+  return { state, loading, error, setSearchText };
+}
+
+export default useTvShowsHomeFetch;
