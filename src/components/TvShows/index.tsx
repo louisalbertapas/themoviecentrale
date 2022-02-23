b9779559f243d1f4324fc9f041ede5601289c2c4
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-02-23 17:01:12 +0800

    Add home page for TV Shows

--- src/components/TvShows/index.tsx
+++ src/components/TvShows/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { BACKDPROP_BASE_URL, IMAGE_BASE_URL } from '../../constants/TmdbApiConstants';
+import useTvShowsHomeFetch from '../../hooks/useTvShowsHomeFetch';
+import ErrorPage from '../ErrorPage';
+import SearchBar from '../SearchBar';
+import Thumbnail from '../Thumbnail';
 
 const TvShows: React.FC = () => {
+  const { state, loading, error, setSearchText } = useTvShowsHomeFetch();
+
+  if (error) return (<ErrorPage />);
+
   return (
-    <div className='mt-5 sm:mt-10'>
-      <div className='py-10 sm:py-20 px-2 sm:px-8 md:px-16 xl:px-40 font-mono'>
-        <div className='px-2 sm:px-8 md:px-16 xl:px-40'>
-          <h1 className='text-4xl sm:text-5xl text-left font-bold'>TV Shows</h1>
+    <div>
+      <>
+        {
+          !loading && state.results[0]
+            ? <img className='mt-20 sm:h-[600px] sm:w-full' src={`${BACKDPROP_BASE_URL}${state.results[0].backdrop_path}`} />
+            : <div className='mt-20 sm:h-[600px] sm:w-full'>Loading...</div>
+        }
+      </>
+      <div className='px-5 sm:px-20 md:px-40 font-mono bg-slate-300 h-full'>
+        <div className='mx-10 px-4'>
+          <div className='text-center -mt-16'>
+            <SearchBar setSearchText={setSearchText} />
+          </div>
+        </div>
+        <div className='mt-5 flex flex-row flex-wrap justify-center'>
+          {
+            state.results.map(tv => (
+              <Thumbnail
+                key={tv.id}
+                source={`${IMAGE_BASE_URL}${tv.poster_path}`}
+                title={tv.name}
+              />
+            ))
+          }
         </div>
       </div>
     </div>
