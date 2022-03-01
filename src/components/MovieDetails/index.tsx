bee3bf984627f322c83e8e6cb0ee3ba35d04e75f
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-03-01 13:14:24 +0800

    Use the useMovieDetailFetch in MovieDetails component

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import useMovieDetailFetch from '../../hooks/useMovieDetailFetch';
+import ErrorPage from '../ErrorPage';
 
 const MovieDetails: React.FC = () => {
   // get the movieId from the query params
   const { movieId } = useParams();
 
+  const { state: movie, error, loading } = useMovieDetailFetch(Number(movieId));
+
+  if (error) return (<ErrorPage />);
+
   return (
-    <div className='mt-32 text-black'>Movie Details of {movieId}</div>
+    <div className='mt-32 text-black'>
+      <>
+        {
+          loading
+            ? <div>Loading...</div>
+            : <div>
+              {movie.title}
+            </div>
+        }
+      </>
+    </div>
   )
 }
 
