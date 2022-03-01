a089f89805d84068aadda01d95646e0661fe8e8b
Louis Albert Apas <louisalbertapas25@gmail.com>
2022-03-01 13:24:07 +0800

    Use the useTvShowDetailFetch in TvShowDetails component

--- src/components/TvShowDetails/index.tsx
+++ src/components/TvShowDetails/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useTvShowDetailFetch from '../../hooks/useTvShowDetailFetch';
+import ErrorPage from '../ErrorPage';
 
 const TvShowDetails: React.FC = () => {
   const { tvShowId } = useParams();
 
+  const { state: tvShow, loading, error } = useTvShowDetailFetch(Number(tvShowId));
+
+  if (error) return (<ErrorPage />);
+
   return (
-    <div className='mt-32 text-black'>TV show details of {tvShowId}</div>
+    <div className='mt-32 text-black'>
+      <>
+      {
+        loading
+          ? <div>Loading...</div>
+          : <div>
+            {tvShow.name}
+          </div>
+      }
+      </>
+    </div>
   )
 }
 
